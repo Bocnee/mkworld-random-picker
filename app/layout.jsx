3449f5f34a94de841e3c3e882b9ecab784180f79
Océane <bocnee.aurel@gmail.com>
2025-06-11 20:00:41 +0200

    :wrench: first settings

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+/* // j'ai préparé le terrain  pour le header et le footer
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+ */
+import "./styles/base/globals.css";
+
+export const metadata = {
+  title: "MK8 : Random Picker",
+  description:
+    "Un générateur aléatoire de combot Kart / Personnages mais aussi de circuit pour Mario Kart 8",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <body>
+        {/*     <Header /> */}
+        {children}
+        {/*     <Footer /> */}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Oh bordel, je ne sais plus coder.</h1>
+      <p> mes tests</p>
+    </>
+  );
+}
