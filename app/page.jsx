f82404e23d6881093e4d90a5ecbbfba511b8f6f9
Océane <bocnee.aurel@gmail.com>
2025-06-14 22:45:50 +0200

    :wrench: tests

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
       <h1>Oh bordel, je ne sais plus coder.</h1>
       <p> mes tests</p>
+      <img src="/illu_chara/illu_chara_mario.png" alt="ada" />
     </main>
   );
 }
