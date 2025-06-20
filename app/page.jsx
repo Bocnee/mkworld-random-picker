965f1c4e60f9e2c770ff780febaa01af9fa5d24e
Océane <bocnee.aurel@gmail.com>
2025-06-20 23:03:53 +0200

    :pen: testing with gallery

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,32 @@
 import Image from "next/image";
 
 export default function Home() {
+  const images = [
+    "/gallery/baby-daisy_in_crowncity001.png",
+    "/gallery/baby-daisy_in_crowncity002.png",
+    "/gallery/baby-daisy_in_crowncity003.png",
+    "/gallery/baby-luigi_bowserscastle001.png",
+    "/gallery/baby-luigi_rainbowroad001.png",
+  ];
+
   return (
     <main>
       <h1>Oh bordel, je ne sais plus coder.</h1>
-      <p> mes tests</p>
-      <img src="/illu_chara/illu_chara_mario.png" alt="ada" />
+      <div className="gallery">
+        {images.map((src, index) => (
+          <div key={index}>
+            <a href={src} target="_blank">
+              {" "}
+              <Image
+                src={src}
+                alt={`Image ${index + 1}`}
+                width={533}
+                height={300}
+              />
+            </a>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
